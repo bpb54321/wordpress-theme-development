47f15d6f2633102d0fe730462cb7ab131d0f7a37
Brian Blosser <bpb54321@gmail.com>
2015-10-01 14:48:35 -0500

    All product info displayed on page

--- wp-content/themes/treehouse-portfolio/index.php
+++ wp-content/themes/treehouse-portfolio/index.php
@@ -7,7 +7,12 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <h1><?php the_title(); ?></h1>
           <p><?php the_content(); ?></p> 
-          <!--<p><?php the_field('product_url'); ?></p> -->      
+          <h2>Related Products</h2>
+          <p><?php the_field('product_title'); ?></p>
+          <p><?php the_field('product_image_url'); ?></p>
+          <p><?php the_field('product_price'); ?></p>
+          <p><?php the_field('product_description'); ?></p>
+
         <?php endwhile; else : ?>
     		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
         <?php endif; ?>

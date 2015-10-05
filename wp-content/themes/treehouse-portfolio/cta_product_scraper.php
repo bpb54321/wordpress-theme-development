15f4f2ec274c5ac81cfe1a3509c0297989c4ecb4
Brian Blosser <bpb54321@gmail.com>
2015-10-05 09:51:17 -0500

    Made some changes to CTA Product Scraper

--- wp-content/themes/treehouse-portfolio/cta_product_scraper.php
+++ wp-content/themes/treehouse-portfolio/cta_product_scraper.php
@@ -2,16 +2,20 @@
 
 function post_updated_do_action( $post_id ) {
 
-    //Get the value of the 'product_url' field
+    //Get the value of the 'product_id' field
     //get_post_meta ( int $post_id, string $key = '', bool $single = false )
     //Return: (mixed) Will be an array if $single is false. Will be value of meta data field if $single is true. 
-    //$product_url_value = get_post_meta($post_id, 'product_url', true );
-    $product_id = get_post_meta($post_id, 'product_id', true );
+    //$product_id = get_post_meta($post_id, 'product_id', false );
+    $product_id = get_post_custom_values('product_id', $post_id);
 
-    //Add data in 'product_url' field to 'product_title' field
-    /* 
-        function update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed $prev_value = '' )
-        Parameters
+    error_log(print_r($product_id,true));
+    die();
+    
+    if ($product_id) {
+        //Add data in 'product_url' field to 'product_title' field
+        
+        /* function update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed $prev_value = '' )
+            Parameters
             $post_id
 
                 (int) (Required) Post ID.
@@ -26,128 +30,152 @@ function post_updated_do_action( $post_id ) {
                 (mixed) (Optional) Previous value to check before removing.
 
                 Default value: ''
-    */
-
-
-    //$product_info = get_product_info($product_url_value);
-    $product_info = get_product_info($product_id);
-    
-    $meta_keys = ['product_title', 'product_image_url', 'product_price', 'product_description']; 
-
-    foreach ($meta_keys as $meta_key) {
-        /*
-        Function: update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed (optional) $prev_value = '' )
-        Return: (int|bool) Meta ID if the key didn't exist, true on successful update, false on failure.
         */
-        update_post_meta($post_id, $meta_key, $product_info[$meta_key]);    
+        $product_info = get_product_info($product_id);
+        
+        $meta_keys = ['product_title', 'product_image_url', 'product_price', 'product_description']; 
+
+        foreach ($meta_keys as $meta_key) {
+            /*
+            Function: update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed (optional) $prev_value = '' )
+            Return: (int|bool) Meta ID if the key didn't exist, true on successful update, false on failure.
+            */
+            update_post_meta($post_id, $meta_key, $product_info[$meta_key]);    
+        }
+    } else {
+        error_log("cta_product_scraper: get_post_meta() could not find a field with the 'product_id' name.");
     }
+
+    
 
 }
 
 add_action( 'save_post', 'post_updated_do_action' );
 
-function get_product_info($product_id) {
+function get_product_info($product_id_string) {
+    //Parse out the comma-separated list of multiple product id's, if present, into an array
+    $product_id_array = explode (",",$product_id_string);
+    /*error_log( print_r( $product_id_array, true ) );
+    die();*/
 
-                //Create url from $product_id
-                $url = "http://www.ctainc.com/product/" . $product_id;
+    // create curl resource, which gets the info from the pages
+    $ch = curl_init();
 
-                //instantiate info array
-                $product_info = array(
-                    "product_title"  => "",
-                    "product_image_url" => "",
-                    "product_price" => "",
-                    "product_description" => "");
-                
-                // create curl resource
-                $ch = curl_init();
+    //instantiate info array
+    $product_info = array(
+        "product_title"  => array(),
+        "product_image_url" => "",
+        "product_price" => "",
+        "product_description" => "");
 
-                // set options
-                curl_setopt($ch, CURLOPT_URL, $url); //set url
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return the transfer as a string
+    //Loop through all product id's
+    foreach ($product_id_array as $product_id) {
+        //Create url from $product_id
+        $url = "http://www.ctainc.com/product/" . $product_id;
+        
+        // set curl options
+        curl_setopt($ch, CURLOPT_URL, $url); //set url
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //return the transfer as a string
 
-                // get html string
-                $html = curl_exec($ch);
+        // get html string
+        $html = curl_exec($ch); 
 
-                // close curl resource to free up system resources
-                curl_close($ch); 
+        //Use DOMDocument class, which is built into php
+        $dom = new DOMDocument;
 
-                //Use DOMDocument class, which is built into php
-                $dom = new DOMDocument;
+        libxml_use_internal_errors(true); //libxml_use_internal_errors() allows you to disable standard libxml errors and enable user error handling. 
+        $dom->loadHTML($html); //loads the html string into a DOMDocument
+    
+        /*----------------------------------Product Title-----------------------------------------*/
+        $product_content_div = find_element_with_class($dom, 'div', 'content');
+        if ($product_content_div == false) {
+            $product_title = "No product title found.";    
+        } else {
+            $target_h1_list = $product_content_div->getElementsByTagName("h1");
+            if ($target_h1_list->length==0) {
+                $product_title = "No product title found.";    
+            } else {
+                //Store the first h1 tag
+                $target_h1 = $target_h1_list[0];
+                $product_title = $target_h1->nodeValue;    
+            }      
+        }
+        
+        //$product_info["product_title"] .= "," . $product_title;
+        $product_info["product_title"] = array($product_info["product_title"],$product_title);
 
-                libxml_use_internal_errors(true); //libxml_use_internal_errors() allows you to disable standard libxml errors and enable user error handling. 
-                $dom->loadHTML($html); //loads the html string into a DOMDocument
+        /*----------------------------------Product Image-----------------------------------------*/
+        $product_image_img = find_element_with_class($dom, 'img', 'product_image');
+        
+        if($product_image_img) {
             
-                
-                /*----------------------------------Product Title-----------------------------------------*/
-                $product_content_div = find_element_with_class($dom, 'div', 'content');
-                if ($product_content_div == false) {
-                    $product_title = "No product title found.";    
-                } else {
-                    $target_h1_list = $product_content_div->getElementsByTagName("h1");
-                    if ($target_h1_list->length==0) {
-                        $product_title = "No product title found.";    
-                    } else {
-                        //Store the first h1 tag
-                        $target_h1 = $target_h1_list[0];
-                        $product_title = $target_h1->nodeValue;    
-                    }      
-                }
-                
-                $product_info["product_title"] = $product_title;
+            $src_attribute = $product_image_img->attributes->getNamedItem('src');
 
-                /*----------------------------------Product Image-----------------------------------------*/
-                $product_image_img = find_element_with_class($dom, 'img', 'product_image');
-                
-                if($product_image_img) {
-                    
-                    $src_attribute = $product_image_img->attributes->getNamedItem('src');
+            if ($src_attribute) {
+                $product_image_url = $src_attribute->value;
+            } else {
+                error_log("cta_product_scraper.php: The <img class='product_image'> had no src attribute. ");
+                $product_image_url = "/";    
+            }
 
-                    if ($src_attribute) {
-                        $product_image_url = $src_attribute->value;
-                    } else {
-                        error_log("cta_product_scraper.php: The <img class='product_image'> had no src attribute. ");
-                        $product_image_url = "/";    
-                    }
+        } else {
+            error_log("cta_product_scraper.php: There was no <img> tag with class 'product_image' ");
+            $product_image_url = "/"; 
+        }
 
-                } else {
-                    error_log("cta_product_scraper.php: There was no <img> tag with class 'product_image' ");
-                    $product_image_url = "/"; 
-                }
+        $product_info["product_image_url"] .= "," . $product_image_url;
+        /*----------------------------Product Price-----------------------------------------*/
+        $product_id = substr($url, -4); //This function requires a url that ends with product id 
+        $price_id = "price_" . $product_id;
+        $price_input_element = $dom->getElementById($price_id); 
 
-                $product_info["product_image_url"] = $product_image_url;
-                /*----------------------------Product Price-----------------------------------------*/
-                $product_id = substr($url, -4); //This function requires a url that ends with product id 
-                $price_id = "price_" . $product_id;
-                $price_input_element = $dom->getElementById($price_id); 
+        if ($price_input_element == NULL) {
+            $product_price = "Price not found.";
+        } else {
+            //Get value of attribute "value"
+            $attributes = $price_input_element->attributes;
+            $value_attribute = $attributes->getNamedItem('value');
+            if ($value_attribute == NULL) { //value attribute doesn't exist
+                $product_price = "Price not found."; 
+            } else {
+                $product_price = $value_attribute->value;    
+            }
+        
+        }
 
-                if ($price_input_element == NULL) {
-                    $product_price = "Price not found.";
-                } else {
-                    //Get value of attribute "value"
-                    $attributes = $price_input_element->attributes;
-                    $value_attribute = $attributes->getNamedItem('value');
-                    if ($value_attribute == NULL) { //value attribute doesn't exist
-                        $product_price = "Price not found."; 
-                    } else {
-                        $product_price = $value_attribute->value;    
-                    }
-                
-                }
+        $product_info["product_price"] .= "," . $product_price;
+        /*----------------------------Product Desc-----------------------------------------*/
+        $product_description_div = find_element_with_class($dom, 'div', 'prod-desc');
 
-                $product_info["product_price"] = $product_price;
-                /*----------------------------Product Desc-----------------------------------------*/
-                $product_description_div = find_element_with_class($dom, 'div', 'prod-desc');
+        if($product_description_div) {
+            //Get first <p> element
+            $target_p_list = $product_description_div->getElementsByTagName("p");
 
-                //Get first <p> element
-                $target_p_list = $product_description_div->getElementsByTagName("p");
+            if ($target_p_list->length == 0) {
+                $product_description = "Product description not found."; 
+            } else {
                 $target_p = $target_p_list[0];
-                $product_description = $target_p->nodeValue;
+                $product_description = $target_p->nodeValue;    
+            }      
+        } else {
+            error_log("cta_product_scraper: There was no product description section of this page.");
+            $product_description = "Product description not found.";
+        }
 
-                $product_info["product_description"] = $product_description;
+        $product_info["product_description"] .= "," . $product_description;
 
-                /*---------------------------------------------------------------------*/
+        /*---------------------------------------------------------------------*/
+        
+    }
+    // close curl resource to free up system resources
+    foreach ($product_info as $property) {
+        $property = substr($property,1);
+        error_log(print_r($property,true));
+    }
 
-                return $product_info;
+    curl_close($ch);
+        
+    return $product_info;        
 
 
 }
@@ -193,7 +221,7 @@ function find_element_with_class($dom, $tag_name, $class_name) {
     }
 
     if(!$found_match) {
-        echo "<p>There were no elements of type " . $tag_name . " with class='" . $class_name . "'</p>";
+        error_log("There were no elements of type " . $tag_name . " with class='" . $class_name);
         return false; 
     }  
 }               

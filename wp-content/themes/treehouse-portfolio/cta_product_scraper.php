bb9fdb235330d34049fe56a37a15dc2c2f54c843
Brian Blosser <bpb54321@gmail.com>
2015-10-05 14:01:11 -0500

    Tested functionality with two product id's, successfully retrieves and stores the product info.

--- wp-content/themes/treehouse-portfolio/cta_product_scraper.php
+++ wp-content/themes/treehouse-portfolio/cta_product_scraper.php
@@ -1,50 +1,114 @@
 <?php
 
+function custom_field_test($post_id) {
+
+    //update_post_meta($post_id, $meta_key, $product_info[$meta_key]);
+    $id_1 = 1998;
+    update_post_meta($post_id, 'product_id', $id_1 );
+
+}
+
 function post_updated_do_action( $post_id ) {
 
+    error_log("Beginning of post_updated_do_action() function");
     //Get the value of the 'product_id' field
     //get_post_meta ( int $post_id, string $key = '', bool $single = false )
     //Return: (mixed) Will be an array if $single is false. Will be value of meta data field if $single is true. 
     //$product_id = get_post_meta($post_id, 'product_id', false );
-    $product_id = get_post_custom_values('product_id', $post_id);
+    $product_ids = get_post_meta($post_id, 'product_id', false );
 
-    error_log(print_r($product_id,true));
-    die();
+    error_log("product_ids array:");
+    error_log(print_r($product_ids,true));
+    /*die();*/
     
-    if ($product_id) {
-        //Add data in 'product_url' field to 'product_title' field
+   
+    if ($product_ids) {
         
-        /* function update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed $prev_value = '' )
-            Parameters
-            $post_id
+        $meta_keys = ['product_title', 'product_image_url', 'product_price', 'product_description'];
 
-                (int) (Required) Post ID.
-            $meta_key
-
-                (string) (Required) Metadata key.
-            $meta_value
-
-                (mixed) (Required) Metadata value. Must be serializable if non-scalar.
-            $prev_value
-
-                (mixed) (Optional) Previous value to check before removing.
-
-                Default value: ''
-        */
-        $product_info = get_product_info($product_id);
+        //Delete all custom fields besides product_id to remove old data
         
-        $meta_keys = ['product_title', 'product_image_url', 'product_price', 'product_description']; 
-
         foreach ($meta_keys as $meta_key) {
-            /*
-            Function: update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed (optional) $prev_value = '' )
-            Return: (int|bool) Meta ID if the key didn't exist, true on successful update, false on failure.
-            */
-            update_post_meta($post_id, $meta_key, $product_info[$meta_key]);    
+            delete_post_meta($post_id, $meta_key);   
         }
-    } else {
+
+        foreach ($product_ids as $product_id) {
+
+            error_log("product_id");
+            error_log(print_r($product_id,true));
+            
+            $product_info = get_product_info($product_id);
+
+            //Check to make sure product_info is what we expect. 
+            error_log("product_info");
+            error_log(print_r($product_info,true));
+            
+            
+
+            
+
+            //Check to see if there are already meta values for each of the $meta_keys
+            //if (/*There is not already an existing meta key*/) {
+                //Create an array of meta keys equal to the size of $product_ids, and populate it with empty strings.
+            //} else /*If the meta key already exists*/ {
+                //Get the array of meta values
+
+                //Check to see whether length($product_ids)==length(meta_key_array)
+                //if(/*length($product_ids)!==length(meta_key_array)*/) {
+
+                //} else {
+
+                //}
+                //What about the situation where the length of $product_ids is not the same as the length of $product_titles, that is, where a new product id has recently been added?
+            //}
+
+            //If there are not yet any meta values for a given key, create them using update_post_meta, and get the corresponding meta_id.
+
+            //If the meta keys already exist:
+            //  Get the current values for the existing meta keys
+            //  Update the values on the meta keys. 
+
+
+            foreach ($meta_keys as $meta_key) {
+                
+                //Function: update_post_meta ( int $post_id, string $meta_key, mixed $meta_value, mixed (optional) $prev_value = '' )
+                //Return: (int|bool) Meta ID if the key didn't exist, true on successful update, false on failure.
+                //update_post_meta($post_id, $meta_key, $product_info[$meta_key]);
+                //Function: add_post_meta($post_id, $meta_key, $meta_value, $unique);
+                //@param: $post_id (integer) (required) The ID of the post to which a custom field should be added. 
+                //@param: $meta_key (string) (required) The key of the custom field which should be added.
+                //@param $meta_value (mixed) (required) The value of the custom field which should be added. If an array is given, it will be serialized into a string.
+                //@param $unique(boolean) (optional) Whether or not you want the key to stay unique. When set to true, the custom field will not be added if the given key already exists among custom fields of the specified post. Default: false
+                //@return: On success, returns the ID of the inserted row, which validates to true. If the $unique argument was set to true and a custom field with the given key already exists, false is returned.  
+                add_post_meta($post_id, $meta_key, $product_info[$meta_key], false);
+
+    
+            }
+        }
+
+        $product_titles = get_post_meta($post_id, 'product_title', false );
+        error_log("product_titles array:");
+        error_log(print_r($product_titles,true));
+
+        $product_image_urls = get_post_meta($post_id, 'product_image_url', false );
+        error_log("product_image_urls array:");
+        error_log(print_r($product_image_urls,true));
+
+        $product_prices = get_post_meta($post_id, 'product_price', false );
+        error_log("product_prices array:");
+        error_log(print_r($product_prices,true));
+
+        $product_descriptions = get_post_meta($post_id, 'product_description', false );
+        error_log("product_descriptions array:");
+        error_log(print_r($product_descriptions,true));
+
+
+    }
+
+    else {
         error_log("cta_product_scraper: get_post_meta() could not find a field with the 'product_id' name.");
     }
+
 
     
 
@@ -52,9 +116,9 @@ function post_updated_do_action( $post_id ) {
 
 add_action( 'save_post', 'post_updated_do_action' );
 
-function get_product_info($product_id_string) {
+function get_product_info($product_id) {
     //Parse out the comma-separated list of multiple product id's, if present, into an array
-    $product_id_array = explode (",",$product_id_string);
+    //$product_id_array = explode (",",$product_id_string);
     /*error_log( print_r( $product_id_array, true ) );
     die();*/
 
@@ -63,13 +127,13 @@ function get_product_info($product_id_string) {
 
     //instantiate info array
     $product_info = array(
-        "product_title"  => array(),
+        "product_title"  => "",
         "product_image_url" => "",
         "product_price" => "",
         "product_description" => "");
 
     //Loop through all product id's
-    foreach ($product_id_array as $product_id) {
+    //foreach ($product_id_array as $product_id) {
         //Create url from $product_id
         $url = "http://www.ctainc.com/product/" . $product_id;
         
@@ -102,7 +166,8 @@ function get_product_info($product_id_string) {
         }
         
         //$product_info["product_title"] .= "," . $product_title;
-        $product_info["product_title"] = array($product_info["product_title"],$product_title);
+        //$product_info["product_title"] = array($product_info["product_title"],$product_title);
+        $product_info["product_title"] = $product_title;
 
         /*----------------------------------Product Image-----------------------------------------*/
         $product_image_img = find_element_with_class($dom, 'img', 'product_image');
@@ -123,7 +188,9 @@ function get_product_info($product_id_string) {
             $product_image_url = "/"; 
         }
 
-        $product_info["product_image_url"] .= "," . $product_image_url;
+        //$product_info["product_image_url"] .= "," . $product_image_url;
+        $product_info["product_image_url"] = $product_image_url;
+
         /*----------------------------Product Price-----------------------------------------*/
         $product_id = substr($url, -4); //This function requires a url that ends with product id 
         $price_id = "price_" . $product_id;
@@ -143,7 +210,8 @@ function get_product_info($product_id_string) {
         
         }
 
-        $product_info["product_price"] .= "," . $product_price;
+        //$product_info["product_price"] .= "," . $product_price;
+        $product_info["product_price"] = $product_price;
         /*----------------------------Product Desc-----------------------------------------*/
         $product_description_div = find_element_with_class($dom, 'div', 'prod-desc');
 
@@ -162,16 +230,17 @@ function get_product_info($product_id_string) {
             $product_description = "Product description not found.";
         }
 
-        $product_info["product_description"] .= "," . $product_description;
+        //$product_info["product_description"] .= "," . $product_description;
+        $product_info["product_description"] = $product_description;
 
         /*---------------------------------------------------------------------*/
         
-    }
+    //}
     // close curl resource to free up system resources
-    foreach ($product_info as $property) {
+    /*foreach ($product_info as $property) {
         $property = substr($property,1);
         error_log(print_r($property,true));
-    }
+    }*/
 
     curl_close($ch);
         
